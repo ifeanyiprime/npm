915687ff46556a60f945e226df0ff89051c55620
Grant <agulueifeanyig@gmail.com>
2024-08-03 16:17:31 +0100

    Fisrt Commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
-import './index.css'
+import "./index.css";
 import Employee from "./components/Employee";
 import { useState } from "react";
 
 function App() {
-   const [role, setRole] = useState('dev'); 
-   const showEmployees = true;
-   return (
-      <div className="App bg-red-300" >
-         {console.log("Inside the return")}
-         {showEmployees ? (
-            <>
-               <input type="text" onChange={(e) => { 
-                  console.log(e.target.value)
-                  setRole(e.target.value)
-               }}/>
-               <Employee name= 'David' role= 'Intern' />
-               <Employee name='Kelvin' role={role } />
-               <Employee name = 'Abby'/>
-            </>
-         ) : (
-            <p>You cannot see the employees</p>
-         )}
-      </div>
-   );
+    const [role, setRole] = useState("dev");
+    const showEmployees = true;
+    return (
+        <div className='App bg-red-300'>
+            {console.log("Inside the return")}
+            {showEmployees ? (
+                <>
+                    <input
+                        type='text'
+                        onChange={(e) => {
+                            console.log(e.target.value);
+                            setRole(e.target.value);
+                        }}
+                    />
+                    <Employee name='David' role='Intern' />
+                    <Employee name='Kelvin' role={role} />
+                    <Employee name='Abby' />
+                </>
+            ) : (
+                <p>You cannot see the employees</p>
+            )}
+        </div>
+    );
 }
 
 export default App;
